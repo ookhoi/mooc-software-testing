c430e32ae26af60e8b828367ab8d342d915ac328
sander <git@humilis.net>
2019-11-19 15:29:04 +0100

    write junit tests for CaesarShiftCipher

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,26 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CaesarShiftCipherTest {
+
+    private CaesarShiftCipher caesarShiftCipher;
+
+    @BeforeEach
+    @DisplayName("Initialize CaesarShiftCipher ()")
+    void CaesarShiftCipher() {
+        caesarShiftCipher = new CaesarShiftCipher();
+    }
+
+    @ParameterizedTest(name = "gword={0}, shift={1}, result={2}")
+    @CsvSource( { "abc, 3, def", "xyz, 3, abc", "sander, 26, sander", "sander, 13, fnaqre" } )
+    @DisplayName("Verify CaesarShiftCipher")
+    void verifyCaesarShiftCipher(String plainText, int shift, String encryptedText) {
+        String result = caesarShiftCipher.CaesarShiftCipher(plainText, shift);
+        assertEquals(encryptedText, result);
+    }
 
 }
