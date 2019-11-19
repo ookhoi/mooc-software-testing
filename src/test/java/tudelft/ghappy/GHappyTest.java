6fd30cd4234c217fbdc45fcbf6985a3060013d9d
sander <git@humilis.net>
2019-11-19 15:10:24 +0100

    write junit tests for GHappy

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,26 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GHappyTest {
+
+    private GHappy gHappy;
+
+    @BeforeEach
+    @DisplayName("Initialize GHappy()")
+    void initializeGHappy() {
+        gHappy = new GHappy();
+    }
+
+    @ParameterizedTest(name = "gword={0}, result={1}")
+    @CsvSource( { "abc,false", "agc,false", "gag,false", "aaggbb, true", "ggaa,true", "aagg, true", "aaggaa,true", "abcd,false", "a, false", "g,false" } )
+    @DisplayName("Verify GG")
+    void verifyGG(String gword, boolean expectedResult) {
+        boolean result = gHappy.gHappy(gword);
+        assertEquals(expectedResult, result);
+    }
+
 }
