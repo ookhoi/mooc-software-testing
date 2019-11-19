9b784a212ca6844f70921c15ffd6e318d3f3dda6
sander <git@humilis.net>
2019-11-19 15:37:22 +0100

    test with space

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -16,7 +16,7 @@ public class CaesarShiftCipherTest {
     }
 
     @ParameterizedTest(name = "gword={0}, shift={1}, result={2}")
-    @CsvSource( { "abc, 3, def", "xyz, 3, abc", "sander, 26, sander", "sander, 13, fnaqre" } )
+    @CsvSource( { "abc, 3, def", "xyz, 3, abc", "sander, 26, sander", "sander, 13, fnaqre", "abc xyz, 3, def abc" } )
     @DisplayName("Verify CaesarShiftCipher")
     void verifyCaesarShiftCipher(String plainText, int shift, String encryptedText) {
         String result = caesarShiftCipher.CaesarShiftCipher(plainText, shift);
