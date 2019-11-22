052e39077a9feebf0606a2f21961758b7fd219fd
sander <git@humilis.net>
2019-11-22 16:40:08 +0100

    Topic 5: Test code smells Quiz topic 5 Practical exercise #1
    implement test

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,22 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MirrorTest {
+
+    private Mirror mirror = new Mirror();
+
+    @ParameterizedTest(name = "Look for \"{1}\" at both the beginning and the end of \"{0}\"")
+    @CsvSource( { "abXYZba,ab", "abca,a", "aba,aba", "abXZba,ab", "abhhhca,a" } )
+    @DisplayName("Look for a mirror image (backwards) string at both the beginning and end of the given string")
+    void lookForMirror(String inString, String mirrorString) {
+        String resultString = mirror.mirrorEnds(inString);
+
+        assertEquals(mirrorString, resultString,
+                "Look for \"{1}\" at both the beginning and the end of \"{0}\"");
+    }
+
 }
